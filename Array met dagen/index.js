b6f37b949c526b71b7989d04739383b80357684c
MertcanYildirim078 <99068314@mydavinci.nl>
2023-02-15 13:51:55 +0100

    Dagen in progress

--- Array met dagen/index.js
+++ Array met dagen/index.js
@@ -1,12 +1,112 @@
 alleDagen = ['maandag', 'dinsdag', 'woensdag', 'donderdag', 'vrijdag', 'zaterdag', 'zondag'];
 
-// console.log('Alle dagen van de week zijn: '); console.log(alleDagen)
+// Hele week hieronder
 
-// console.log('De werkdagen zijn:')
+console.log('Alle dagen van de week zijn:')
+
+for(i=0; i<7; i++) {
+    let kommaOfPunt = ''
+    if (i<6) {
+         kommaOfPunt = ','
+    }
+
+    else {
+        kommaOfPunt = '.'
+    }
+    
+    console.log(alleDagen[i] + kommaOfPunt)
+   
+}
+
+console.log('')
+
+// Weekdagen hieronder
+
+console.log('De werkdagen zijn:')
 
 for(i=0; i<5; i++) {
     let kommaOfPunt = ''
     if (i<4) {
+         kommaOfPunt = ','
+    }
+
+    else {
+        kommaOfPunt = '.'
+    }
+    
+    console.log(alleDagen[i] + kommaOfPunt)
+   
+}
+
+console.log('')
+
+// Weekenddagen hieronder
+
+console.log('De weekenddagen zijn:')
+
+for(i=5; i<7; i++) {
+    let kommaOfPunt = ''
+    if (i<6) {
+         kommaOfPunt = ','
+    }
+
+    else {
+        kommaOfPunt = '.'
+    }
+    
+    console.log(alleDagen[i] + kommaOfPunt)
+   
+}
+
+console.log('')
+
+// Alle dagen van de week in omgekeerde volgorde hieronder
+
+console.log('Alle dagen van de week in omgekeerde volgorde zijn:')
+
+for(i=-1; i>-7; i--) {
+    let kommaOfPunt = ''
+    if (i<-6) {
+         kommaOfPunt = ','
+    }
+
+    else {
+        kommaOfPunt = '.'
+    }
+    
+    console.log(alleDagen[i] + kommaOfPunt)
+   
+}
+
+console.log('')
+
+// De werkdagen in omgekeerde volgorde hieronder
+
+console.log('De werkdagen in omgekeerde volgorde zijn:')
+
+for(i=-1; i>-7; i--) {
+    let kommaOfPunt = ''
+    if (i<-6) {
+         kommaOfPunt = ','
+    }
+
+    else {
+        kommaOfPunt = '.'
+    }
+    
+    console.log(alleDagen[i] + kommaOfPunt)
+   
+}
+
+console.log('')
+
+// De weekenddagen in omgekeerde volgorde hieronder
+
+console.log('De weekenddagen in omgekeerde volgorde zijn:')
+
+for(i=-1; i>-7; i--) {
+    let kommaOfPunt = ''
+    if (i<-6) {
          kommaOfPunt = ','
     }
 
