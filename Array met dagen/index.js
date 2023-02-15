294eebf8b86a038eed4e477a9fda2006d3a43a6f
MertcanYildirim078 <99068314@mydavinci.nl>
2023-02-15 12:18:37 +0100

    Dagen updated en begin

--- Array met dagen/index.js
+++ Array met dagen/index.js
@@ -1,3 +1,19 @@
 alleDagen = ['maandag', 'dinsdag', 'woensdag', 'donderdag', 'vrijdag', 'zaterdag', 'zondag'];
 
-console.log('Alle dagen van de week zijn: '); console.log(alleDagen)
+// console.log('Alle dagen van de week zijn: '); console.log(alleDagen)
+
+// console.log('De werkdagen zijn:')
+
+for(i=0; i<5; i++) {
+    let kommaOfPunt = ''
+    if (i<4) {
+         kommaOfPunt = ','
+    }
+
+    else {
+        kommaOfPunt = '.'
+    }
+    
+    console.log(alleDagen[i] + kommaOfPunt)
+   
+}
