3531aeefdfaffbe4aa680bcb2ffddfe87dacbe4d
MertcanYildirim078 <99068314@mydavinci.nl>
2023-02-22 11:41:21 +0100

    Begin rekenen met arrays

---
+++ Rekenen met arrays/index.js
@@ -0,0 +1,16 @@
+var array1 = [1,2,3,4,5,6,7,8,9,10]
+var array2 = [2,4,6,8,10,12,14,16,18,20]
+
+function arrayOptellen() {
+    for(i=0; i==array1.length; i++) {
+        var opgeteld = array1[i] + array2[i]
+        document.write(opgeteld1)
+    }
+}
+
+
+document.write('<h1>Array operatoren</h1>')
+
+document.write('<h2>Optellen met 2 operatoren zijn:</h2>')
+
+arrayOpgeteld = arrayOptellen()
