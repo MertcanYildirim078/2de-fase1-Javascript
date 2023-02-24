53e4861b947929f1a63c36771d33291b524906c5
MertcanYildirim078 <99068314@mydavinci.nl>
2023-02-24 11:10:49 +0100

    Rekenen met arrays af

--- Rekenen met arrays/index.js
+++ Rekenen met arrays/index.js
@@ -1,16 +1,48 @@
-var array1 = [1,2,3,4,5,6,7,8,9,10]
-var array2 = [2,4,6,8,10,12,14,16,18,20]
+const array1 = [1,2,3,4,5,6,7,8,9,10]
+const array2 = [2,4,6,8,10,12,14,16,18,20]
 
 function arrayOptellen() {
-    for(i=0; i==array1.length; i++) {
-        var opgeteld = array1[i] + array2[i]
-        document.write(opgeteld1)
-    }
+    
+    for(let i=0; i < array1.length; i++) {
+        document.write(array1[i], ' + ', array2[i], ' = ', array1[i] + array2[i], '<br>' );
+    } 
 }
 
+function arrayAftrekken() {
+    
+    for(let i=0; i < array1.length; i++) {
+        document.write(array1[i], ' - ', array2[i], ' = ', array2[i] - array1[i], '<br>' );
+    } 
+}
+
+function arrayVermenigvuldigen() {
+    
+    for(let i=0; i < array1.length; i++) {
+        document.write(array1[i], ' * ', array2[i], ' = ', array1[i] * array2[i], '<br>' );
+    } 
+}
+
+function arrayDelen() {
+    
+    for(let i=0; i < array1.length; i++) {
+        document.write(array1[i], ' / ', array2[i], ' = ', array2[i] / array1[i], '<br>' );
+    } 
+}
 
 document.write('<h1>Array operatoren</h1>')
 
 document.write('<h2>Optellen met 2 operatoren zijn:</h2>')
 
 arrayOpgeteld = arrayOptellen()
+
+document.write('<h2>Aftrekken met 2 operatoren zijn:</h2>')
+
+arrayafgetrokken = arrayAftrekken()
+
+document.write('<h2>Vermenigvuldigen met 2 operatoren zijn:</h2>')
+
+arrayVermeigvuldigd = arrayVermenigvuldigen()
+
+document.write('<h2>Delen met 2 operatoren zijn:</h2>')
+
+arraygedeeld = arrayDelen()
